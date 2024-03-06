a70d48a301e8fd4fdbb05c9dce9c35429567f09a
David Shneyderman <shneydermand@wit.edu>
2024-03-06 18:17:06 -0500

    Frontend basics done

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-    cors({ 
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+app.use(cors());
+// app.use(
+//     cors({ 
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (req,res)=>{
     console.log(req);
